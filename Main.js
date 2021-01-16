279851f19dda24553c2f5f550b04c5067eda3af8
Sanaya26 <73732178+Sanaya26@users.noreply.github.com>
2021-01-16 10:48:23 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,23 @@
+console.log ("My fist Console Project"); 
+var firstname= "Sanaya";
+var lastname= "Shetty";
+var fullname= firstname.concat (lastname);
+console.log (fullname);
+var s= 10;
+var n= 20;
+var y= s+n;
+console.log ("Addition is"+ y);
+ y= s*n;
+console.log ("Multiplication is"+ y);
+y= s/n;
+console.log ("Division is"+ y);
+y= s-n;
+console.log ("Subtraction"+ y);
+
+
+
+
+
+
+
+
